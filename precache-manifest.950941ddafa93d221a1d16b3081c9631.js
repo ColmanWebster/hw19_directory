1c595c5c9054973b3736992c80f5998eec412783
Colman Webster <roguefiend@yahoo.com>
2021-02-09 17:48:43 -0800

    Updates

---
+++ precache-manifest.950941ddafa93d221a1d16b3081c9631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e32663a00f34317543836dffe9d7ad",
+    "url": "/hw19_directory/index.html"
+  },
+  {
+    "revision": "696c5dd20e29caedf9b5",
+    "url": "/hw19_directory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c59cbff2334d3fa1064d",
+    "url": "/hw19_directory/static/js/2.2ba8b590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw19_directory/static/js/2.2ba8b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696c5dd20e29caedf9b5",
+    "url": "/hw19_directory/static/js/main.8df9fcc8.chunk.js"
+  },
+  {
+    "revision": "5cef99c6471c86f5fe60",
+    "url": "/hw19_directory/static/js/runtime-main.0500926c.js"
+  }
+]);
